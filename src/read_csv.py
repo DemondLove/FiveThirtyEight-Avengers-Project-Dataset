c0a08a6b26c6d542581c90c3c9262814bea71a15
Demond Love <delove@my.bellevue.edu>
2018-04-21 16:38:24 -0500

    Update read_csv.py
    Update read_csv.py

--- src/read_csv.py
+++ src/read_csv.py
@@ -1,8 +1,9 @@
 import csv
 
-with open ('/Users/Love/Documents/GitHub/msds510/data/interim/avengers_utf8.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    lines = []
-    for line in csv_reader:
-        lines.append(line)
-    print (lines[161])
+def job(robin):
+    with open(robin, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        lines = []
+        for line in csv_reader:
+            lines.append(line)
+        print (lines[161])
