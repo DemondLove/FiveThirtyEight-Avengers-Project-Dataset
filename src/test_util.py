97e46fcc278c41bafa3bd8f767baf6e756bfca13
Demond Love <delove@my.bellevue.edu>
2018-05-20 16:01:34 -0500

    Create get_value & test
    Created get_value function and tested it with given test set.

--- src/test_util.py
+++ src/test_util.py
@@ -1,3 +1,18 @@
 if __name__ == '__main__':
-    import util
-    print (util.to_int('nine'))
+    from util import get_value
+    x = {'a': 1, 'b': 52, 'd': 6}
+    y = ['a', 'c', 'd']
+    print(get_value(x, 'q'))
+# Out[1]:  None
+
+    print(get_value(x, 'a'))
+# Out[2]:  1
+
+    print(get_value(x, 'b'))
+# Out[3]:  52
+
+    print(get_value(y, 'b'))
+# Out[4]:  None
+
+    print(get_value(y, 'd'))
+# Out[5]:  2
--- src/util.py
+++ src/util.py
@@ -2,11 +2,40 @@ def to_int(x):
     """
     Converts an input value into an integer
 
-    :return:
-    int
+    :param x: float
+    :return: int
     """
     try:
         return int(x)
     except:
         return None
 
+
+def get_value(items, value):
+    """
+    If the items are a list or tuple, the function should return the integer index of where
+    the value is in the sequence.
+
+    If the items are a dictionary, it should return the value associated with the input key.
+
+    :param items: tuple, list, or dict
+    :param value: index if tuple or list; value if dict
+    :return:
+    """
+    if type(items) == tuple:
+        if value in items:
+            return items.index(value)
+        else:
+            return None
+    elif type(items) == list:
+        if value in items:
+            return items.index(value)
+        else:
+            return None
+    elif type(items) == dict:
+        if value in items:
+            return items[value]
+        else:
+            return None
+    else:
+        return None
