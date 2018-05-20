055f39302a599814ce6ca82d791cac6a3e668aaf
Demond Love <delove@my.bellevue.edu>
2018-05-20 15:27:08 -0500

    Create to_int function  & test
    Created a to_int function, which converts a value to an int, and returns None if it cannot, due to some python error. I tested this with test_until.py, and I was able to successfully convert 4.0 to 4, then received None instead of error message when inputting 'nine'.

---
+++ src/test_util.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    import util
+    print (util.to_int('nine'))
---
+++ src/util.py
@@ -0,0 +1,12 @@
+def to_int(x):
+    """
+    Converts an input value into an integer
+
+    :return:
+    int
+    """
+    try:
+        return int(x)
+    except:
+        return None
+
