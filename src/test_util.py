9056abf5636538e4d1139b7a7e6e4176b9d83638
Demond Love <delove@my.bellevue.edu>
2018-05-20 18:02:11 -0500

    Create UnitTest Cases
    Create UnitTest Cases

--- src/test_util.py
+++ src/test_util.py
@@ -5,32 +5,41 @@ if __name__ == '__main__':
 
     from util import to_int
 
-    class Testto_int(unittest.TestCase):
+    import msds510.utils
+
+    class TestToInt(unittest.TestCase):
 
         def test_float(self):
-            #Test a float to return int
             self.assertEqual(to_int(4.0), 4)
+
         def test_invalid_str(self):
             self.assertAlmostEqual(to_int('nine'), None)
+
         def test_int_input(self):
             self.assertAlmostEqual(to_int(8), 8)
-    #
-    # class Testget_value(unittest.TestCase):
-    #     x = {'a': 1, 'b': 52, 'd': 6}
-    #     y = ['a', 'c', 'd']
-    #     pass
 
-# print(get_value(x, 'q'))
-# # Out[1]:  None
-#
-# print(get_value(x, 'a'))
-# # Out[2]:  1
-#
-# print(get_value(x, 'b'))
-# # Out[3]:  52
-#
-# print(get_value(y, 'b'))
-# # Out[4]:  None
-#
-# print(get_value(y, 'd'))
-# Out[5]:  2
+    class TestGetValue(unittest.TestCase):
+        x = {'a': 1, 'b': 52, 'd': 6}
+        y = ['a', 'c', 'd']
+
+        def test_dict_NoValue(self):
+            self.assertAlmostEqual(get_value(x, 'q'), None)
+
+        def test_dict_valid(self):
+            self.assertAlmostEqual(get_value(x, 'a'), 1)
+
+        def test_dict_valid(self):
+            self.assertAlmostEqual(get_value(x, 'b'), 52)
+
+        def test_list_NoValue(self):
+            self.assertAlmostEqual(get_value(y, 'b'), None)
+
+        def test_list_value(self):
+            self.assertAlmostEqual(get_value(y, 'd'), 2)
+
+        def test_missinginputs(self):
+            self.assertRaises(NameError, get_value())
+    class TestGetDateJoined(unittest.TestCase):
+
+        def test_correct(self):
+            self.assertEquals((msds510.utils.get_date_joined('1968', 'Nov-68'), '1968-11-01')
